87b56c6c4a900426fc67eaf6d9446cc300b741ae
Samuel Lima <samuelshigen@gmail.com>
2020-01-06 18:38:49 -0300

    Front end em react

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Routes from './routes';
+import './style.css';
+import Header from './components/Header';
+
+const App = () => (
+<div className="App">
+      <Header/>
+      <Routes/>
+    </div>
+
+)
+export default App;
